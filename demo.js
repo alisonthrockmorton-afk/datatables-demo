216a36b37080afbdd5596ad4e675655e98fa3f61
alisonthrockmorton-afk <alison.throckmorton@sjsu.edu>
2026-05-19 12:01:06 -0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -49,7 +49,7 @@ console.log($(this).attr('value'));
   $('#googleSheetsDataTable').html('<table cellpadding="0" cellspacing="0" border="0" class="display table" id="data-table-container" style="width:100%"></table>');
 var url = 'https://sheets.googleapis.com/v4/spreadsheets/1QzT3tUeBBd7R7NWbpVIPM9G_fI_DH2knKKZeP82PDY0/values/A:G?key=AIzaSyCmqnBijhOsTPfft3WE6rYAfQ1tERXPoAg';
 if (window.location.href.indexOf("accountability-dashboard") > -1) {
-    url = 'https://sheets.googleapis.com/v4/spreadsheets/1A_o9Nk-COZl21FqTKgcwhS_EU1wo3reEMvufj3fovWc/values/A:G??key=AIzaSyCmqnBijhOsTPfft3WE6rYAfQ1tERXPoAg';
+    url = 'https://sheets.googleapis.com/v4/spreadsheets/1A_o9Nk-COZl21FqTKgcwhS_EU1wo3reEMvufj3fovWc/values/A:G?key=AIzaSyCmqnBijhOsTPfft3WE6rYAfQ1tERXPoAg';
 } 
 //this function creates the datatable and selects configuration options
   var oTable = $('#data-table-container').DataTable({
