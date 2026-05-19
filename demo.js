3e8c8f35b3fb423ce0b3f6aaeee7f4ab1ef8cca4
alisonthrockmorton-afk <alison.throckmorton@sjsu.edu>
2026-05-19 11:34:52 -0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -47,7 +47,7 @@ console.log($(this).attr('value'));
 }
   // create the table container and object
   $('#googleSheetsDataTable').html('<table cellpadding="0" cellspacing="0" border="0" class="display table" id="data-table-container" style="width:100%"></table>');
-var url = 'https://sheets.googleapis.com/v4/spreadsheets/1QzT3tUeBBd7R7NWbpVIPM9G_fI_DH2knKKZeP82PDY0/values/A:G?key=AIzaSyCmqnBijhOsTPfft3WE6rYAfQ1tERXPoAg';
+var url = 'https://sheets.googleapis.com/v4/spreadsheets/1vYYkibhdLFzgaqPbjAQ5NY-cccVW-w_W3C9kK9f3gD4/values/A:G?key=AIzaSyCmqnBijhOsTPfft3WE6rYAfQ1tERXPoAg';
 if (window.location.href.indexOf("accountability-dashboard") > -1) {
     url = 'https://sheets.googleapis.com/v4/spreadsheets/1A_o9Nk-COZl21FqTKgcwhS_EU1wo3reEMvufj3fovWc/values/A:G?key=AIzaSyCmqnBijhOsTPfft3WE6rYAfQ1tERXPoAg';
 } 
